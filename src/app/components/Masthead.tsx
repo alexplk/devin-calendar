f1b57983b4c1ec969b5ec24e291c4e03e6ec99a2
Alex Poliakov <alexp6v@gmail.com>
2026-01-30 21:00:57 -0500

    Header smaller

--- src/app/components/Masthead.tsx
+++ src/app/components/Masthead.tsx
@@ -1,7 +1,7 @@
 export function Masthead() {
   return (
-    <header className="py-16 text-center mb-2">
-      <h1 className="text-5xl font-bold text-gray-900 dark:text-white">
+    <header className="py-10 text-center">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
         Devin's Calendar
       </h1>
     </header>
